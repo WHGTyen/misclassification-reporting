d2367fd015345491155a5508ca07941b7c6dd859
WHGTyen <gladys.tyen@gmail.com>
2018-10-30 10:34:21 +0000

    Added requested change

--- initialise.ts
+++ initialise.ts
@@ -1,60 +1,68 @@
 const numberOfMessages = 100
 
+function addRow(table: HTMLElement, columns: HTMLElement[]) {
+    let row = document.createElement("tr")
+    for (let column of columns) {
+        row.appendChild(column)
+    }
+}
+
+function createColumn(id: string): HTMLElement {
+    let column = document.createElement("th")
+    column.setAttribute("id", id)
+    return column
+}
+
+function addText(element: HTMLElement, text: string) {
+    let textNode = document.createTextNode(text)
+    element.appendChild(textNode)
+}
+
+function addRadioButton(element: HTMLElement, text: string, name: string) {
+    let radioButton = document.createElement("input")
+    radioButton.setAttribute("type", "radio")
+    radioButton.setAttribute("value", text)
+    radioButton.setAttribute("name", name)
+    element.appendChild(radioButton)
+}
+
+function addInputField(element: HTMLElement, name: string) {
+    let inputField = document.createElement("input")
+    inputField.setAttribute("type", "text")
+    inputField.setAttribute("name", name)
+    element.appendChild(inputField)
+}
+
 for (let number = 0; number < numberOfMessages; number++) {
     // <th id="message">
     //     Message
     // </th>
-    let messageColumn = document.createElement("th")
-    messageColumn.setAttribute("id", "message")
-    let messageText = document.createTextNode(generateMessage())
-    messageColumn.appendChild(messageText)
+    let messageColumn = createColumn("message")
+    addText(messageColumn, generateMessage())
 
     // <th id="label">
     //     Related or Unrelated
     // </th>
-    let labelColumn = document.createElement("th")
-    labelColumn.setAttribute("id", "label")
-    let labelText = document.createTextNode(pickRandomLabel(["Related", "Unrelated"]))
-    labelColumn.appendChild(labelText)
+    let labelColumn = createColumn("label")
+    addText(messageColumn, pickRandomLabel(["Related", "Unrelated"]))
 
     // <th id="tick">
     //     <input type="radio" name="verification" value="yes">Yes
     // </th>
-    let tickColumn = document.createElement("th")
-    tickColumn.setAttribute("id", "tick")
-    let tickRadio = document.createElement("input")
-    tickRadio.setAttribute("type", "radio")
-    tickRadio.setAttribute("name", "verification")
-    tickRadio.setAttribute("value", "yes")
-    tickColumn.appendChild(tickRadio)
-    let tickText = document.createTextNode("Yes")
-    tickColumn.appendChild(tickText)
+    let tickColumn = createColumn("tick")
+    addRadioButton(tickColumn, "yes", "verification")
+    addText(tickColumn, "Yes")
 
     // <th id="comment">
     //     <input type="radio" name="verification" value="no">No, because: <input type="text" name="comment">
     // </th>
-    let commentColumn = document.createElement("th")
-    commentColumn.setAttribute("id", "comment")
-    let commentRadio = document.createElement("input")
-    commentRadio.setAttribute("type", "radio")
-    commentRadio.setAttribute("name", "verification")
-    commentRadio.setAttribute("value", "no")
-    commentColumn.appendChild(commentRadio)
-    let commentText = document.createTextNode("No, because: ")
-    commentColumn.appendChild(commentText)
-    let commentInput = document.createElement("input")
-    commentInput.setAttribute("type", "text")
-    commentInput.setAttribute("name", "comment")
-    commentColumn.appendChild(commentInput)
-
-    let row = document.createElement("tr")
-    row.appendChild(messageColumn)
-    row.appendChild(labelColumn)
-    row.appendChild(tickColumn)
-    row.appendChild(commentColumn)
+    let commentColumn = createColumn("comment")
+    addRadioButton(commentColumn, "no", "verification")
+    addText(commentColumn, "No, because: ")
+    addInputField(commentColumn, "comment")
 
     let table = document.getElementById("table")
-    table.appendChild(row)
+    addRow(table, [messageColumn, labelColumn, tickColumn, commentColumn])
 }
 
 
